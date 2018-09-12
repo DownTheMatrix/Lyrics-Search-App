d3cc8031238e717ded40e43f3ff667924378299f
DownTheMatrix <suija87@gmail.com>
2018-09-12 19:13:06 +0200

    First update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+
+/* Components */
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Welcome to Lyrics Search
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
