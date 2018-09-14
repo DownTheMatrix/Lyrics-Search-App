48fe1739c8d8e4f1eebb87a14a155a52e60416e1
DownTheMatrix <suija87@gmail.com>
2018-09-14 23:28:33 +0200

    Update function

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -51,7 +51,7 @@ class Lyrics extends Component {
           </Link>
           <div className="card">
             <h5 className="card-header">
-              {track.track_name} by{' '}
+              { track.track_name } by{' '}
               <span className="text-secondary">{ track.artist_name }</span>
             </h5>
             <div className="card-body">
